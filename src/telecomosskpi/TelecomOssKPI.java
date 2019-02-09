8824fb15de5e4e1e88cde9b16c8bb268ece21289
saifulislammithu <saifulrubd@gmail.com>
2019-02-10 00:49:06 +0600

    Message print

--- src/telecomosskpi/TelecomOssKPI.java
+++ src/telecomosskpi/TelecomOssKPI.java
@@ -15,7 +15,7 @@ public class TelecomOssKPI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        system.out.println("OSS KPI ");
     }
     
 }
